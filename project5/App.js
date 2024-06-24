3ef33ad9afbbe5c675478d4b635317aaffd0cdfa
KBCyber <aninyung@st.ug.edu.gh>
2024-06-24 14:18:10 +0000

    Update

--- project5/App.js
+++ project5/App.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home</Text>
+      <Text>Txt</Text>
+      <Text>Menu</Text>
+      <Text>Settings</Text>
+      <Text>Cards</Text>
+      <Text>Statistics</Text>
+      <Text>Navigation</Text>
       <StatusBar style="auto" />
     </View>
   );
